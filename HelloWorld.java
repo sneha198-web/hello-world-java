1ef998ff70b1b8bb2e930bcde5f23a337e12ef92
Bhupalam Sneha <BH20332383@wipro.com>
2025-05-08 17:25:04 +0530

    updated

--- HelloWorld.java
+++ HelloWorld.java
@@ -36,6 +36,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("this is madhu");
-    System.out.println("HI 43 batch devops");
+    System.out.println("this is sneha");
   }
 }
